c157576c9518cdfe67154d80ffc5c1428246c671
Noé <noelevanquang@gmail.com>
2022-02-03 15:20:59 +0100

    "ordi portable commit ajout d'une classe"

--- obadge_site/index.py
+++ obadge_site/index.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 import flask
 # importation de deux modules pour la barre de progression
 # import alive_progress 
@@ -11,20 +10,20 @@ class code_accueil:
         self.code_HTML_principal = code_HTML_principal
 
     @app.route("/") # répertoire du site
-    def Accueil(code_html):
+    def Accueil():
     # titre du site
         nom_du_site = {"nomsite": "Openbadge"}
         return flask.render_template("index.html", nom_du_site = nom_du_site)
 
 class heritage_de_code_accueil(code_accueil):
-    def __init__(self, code_HTML_principal, code_HTML_herite):
-        heritage_de_code_accueil.__init__(code_HTML_principal)
+    def __init__(self, code_HTML_Accueil, code_HTML_herite):
+        code_accueil.__init__(self, code_HTML_Accueil)
         self.code_HTML_herite = code_HTML_herite
 
     @app.route("/")
-    def heritage(code_herite_1):
+    def heritage():
         categories = {"Home": "Accueil", "Officience_groupe": "Officience Team", "collection_badges": "Badges Collection"}
         return flask.render_template("partie_construction.html", categories = categories)
 
-html_code = code_accueil(code_accueil = code_accueil, heritage_de_code_accueil = heritage_de_code_accueil)
+html_code = heritage_de_code_accueil(heritage_de_code_accueil.heritage, code_accueil.Accueil)
 print(html_code)
