e8e2297cadf5d9c5c54c438a6fe821a6f7e42e44
Noé <noelevanquang@gmail.com>
2022-02-10 12:49:55 +0100

    "modif fichier python problem, index problem et autres à résoudre"

--- obadge_site/index.py
+++ obadge_site/index.py
@@ -15,23 +15,22 @@ class code_accueil:
         nom_du_site = {"nomsite": "Openbadge"}
     # categories du site
         categories = {"Home": "Accueil", "Officience_groupe": "Officience Team", "Collection_badges": "Badges Collection"}
-    # login
-        login = {"Authentification": "Login"}
-    # inscription
-        inscription = {"Authentification": "Sign Up"}
-        return flask.render_template("index.html", nom_du_site = nom_du_site, categories = categories, login = login, inscription = inscription)
+        return flask.render_template("index.html", nom_du_site = nom_du_site, categories = categories)
 
-html_code = code_accueil(code_accueil.Accueil)
-print(html_code)
-
-'''
 class heritage_de_code_accueil(code_accueil):
-    def __init__(self, code_HTML_Accueil, code_HTML_herite):
-        code_accueil.__init__(self, code_HTML_Accueil)
+    def __init__(self, code_HTML_principale, code_HTML_herite):
+        code_accueil.__init__(self, code_HTML_principale)
         self.code_HTML_herite = code_HTML_herite
 
-    @app.route("/")
-    def heritage():
-        categories = {"Home": "Accueil", "Officience_groupe": "Officience Team", "Collection_badges": "Badges Collection"} # appel de ce dictionnaire dans "partie_construction.html"
-        return flask.render_template("partie_construction.html", categories = categories)
-'''
+    @app.route("/static")
+    def login():
+        login = {"Authentification": "Login"}
+        return flask.render_template("login.html", login = login)
+
+    @app.route("/static")
+    def Inscription():
+        inscription = {"Authentification": "Sign Up"}
+        return flask.render_template("inscription.html", inscription = inscription)
+
+html_code = code_accueil(heritage_de_code_accueil)
+print(html_code)
