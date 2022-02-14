dc0e521f15777272c7216cdbcabb729af591824a
Noé <noelevanquang@gmail.com>
2022-02-14 12:59:28 +0100

    "modification login / signup | ajout / modification pages html | modification index.py / ajout DB | modif / ajout CSS"

--- obadge_site/index.py
+++ obadge_site/index.py
@@ -1,9 +1,17 @@
 import flask
+import flaskext.mysql
 # importation de deux modules pour la barre de progression
 # import alive_progress
 # import time
 
 app = flask.Flask(__name__)
+
+# mysql configuration
+mysql = flaskext.mysql.MySQL()
+app.config['MYSQL_HOST'] = 'sql102.byethost18.com'
+app.config['MYSQL_USER'] = 'b18_31066232'
+app.config['MYSQL_PASSWORD'] = 'noenoenoe93100'
+app.config['MYSQL_DB'] = 'b18_31066232_obadge_officience'
 
 
 class code_accueil:
@@ -21,6 +29,14 @@ class code_accueil:
     @app.route("/groupe")
     def Team():
         return flask.render_template("groupe.html")
+
+    @app.route("/badges")
+    def Badges():
+        return flask.render_template("badges.html")
+
+    @app.route("/login")
+    def Login():
+        return flask.render_template("login.html")
 
 
 html_code = code_accueil(code_HTML_principal=code_accueil)
