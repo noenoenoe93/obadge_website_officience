ef57daefdb0bea50159b9129c87bdb9cd64b85b2
Noé <noelevanquang@gmail.com>
2022-02-17 12:51:24 +0100

    "reduce and fix python code | fix CSS code / HTML"

--- obadge_site/index.py
+++ obadge_site/index.py
@@ -1,53 +1,52 @@
-import flask
-import flaskext.mysql
-import flask_mysqldb
+from flask import render_template as tmp
+from flask import request as rq
+from flask import Flask
+from flask_mysqldb import MySQL
+# import flaskext.mysql
 # importation de deux modules pour la barre de progression
 # import alive_progress
 # import time
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 # mysql configuration
-mysql = flaskext.mysql.MySQL()
-app.config['MYSQL_HOST'] = 'sql102.byethost18.com'
-app.config['MYSQL_USER'] = 'b18_31066232'
-app.config['MYSQL_PASSWORD'] = 'noenoenoe93100'
-app.config['MYSQL_DB'] = 'obadge_officience_database'
+mysql = MySQL()
+app.config['MYSQL_HOST'] = 'remotemysql.com'
+app.config['MYSQL_USER'] = '9chqeV2qiY'
+app.config['MYSQL_PASSWORD'] = 'KijTw9vZN4'
+app.config['MYSQL_DB'] = '9chqeV2qiY'
+
+# initialisation mysql
+mysql.init_app(app)
+
+@app.route("/")  # répertoire du site
+def Accueil():
+    return tmp("home.html")
 
 
-class code_accueil:
-    def __init__(self, code_HTML_principal):
-        self.code_HTML_principal = code_HTML_principal
+@app.route("/inscription", methods=['POST', 'GET'])
+def Inscription():
+    if rq.method == "GET":
+        return tmp("inscription.html")
+    if rq.method == "POST":
+        name = rq.form.get("name")
+        password = rq.form.get("password")
+        email = rq.form.get("email")
+        cur = mysql.connection.cursor()  # connexion à la base de données
+        cur.execute("INSERT INTO inscription VALUES (%s, %s, %s)", (name, password, email))  # exécution de la requête mysql
+        cur.close()
+        return tmp("inscription.html", name=name, password=password, email=email)
 
-    @app.route("/")  # répertoire du site
-    def Accueil():
-        return flask.render_template("home.html")
-
-    @app.route("/inscription", methods=["POST", "GET"])
-    def Inscription():
-        if flask.request.method == "GET":
-            return flask.render_template("inscription.html")
-        if flask.request.method == "POST":
-            name = flask.request.form.get("name")
-            password = flask.request.form.get("password")
-            email = flask.request.form.get("email")
-            cur = flask_mysqldb.MySQL.connection.cursor() # connexion à la base de données
-            flask_mysqldb.MySQL.connection.execute("INSERT INTO inscription VALUES (%s, %s, %s)",(name, password, email))  # exécution de la requête mysql
-            flask_mysqldb.MySQL.connection.cursor.close()
-            return flask.render_template("inscription.html", name=name, password=password, email=email)
-
-    @app.route("/groupe")
-    def Team():
-        return flask.render_template("groupe.html")
-
-    @app.route("/badges")
-    def Badges():
-        return flask.render_template("badges.html")
-
-    @app.route("/login")
-    def Login():
-        return flask.render_template("login.html")
+@app.route("/groupe")
+def Team():
+    return tmp("groupe.html")
 
 
-html_code = code_accueil(code_HTML_principal=code_accueil)
-print(html_code)
+@app.route("/badges")
+def Badges():
+    return tmp("badges.html")
+
+
+@app.route("/login")
+def Login():
+    return tmp("login.html")
