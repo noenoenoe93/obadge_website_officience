dee1ca01741088b75d6d3ccbc96ca742c69ea716
Noé <noelevanquang@gmail.com>
2022-03-01 16:36:31 +0100

    "modif / fix index.py | modif css code | add html file / modif html file"

--- obadge_site/index.py
+++ obadge_site/index.py
@@ -1,14 +1,16 @@
-from flask import render_template as tmp
+from unicodedata import name
+from flask import render_template as tmp, session
 from flask import request as rq
 from flask import Flask as flk
 from flask import flash as fls
 from flask import redirect as rdir
 from flask import url_for as rlf
+from flask import session as ses
+from flask_session import Session as flk_ses
 from flask_mysqldb import MySQL as msl
 from wtforms import Form as fm
 from wtforms import StringField as stf
 from wtforms import PasswordField as psf
-# from wtforms import validators as vld
 from wtforms import EmailField as emf
 from wtforms import SubmitField as sbm
 from wtforms.validators import DataRequired as dt
@@ -17,6 +19,9 @@ from wtforms.validators import Email as em
 from wtforms.validators import EqualTo as eq
 
 app = flk(__name__)
+
+# session configuration
+app.config["SESSION_PERMANENT"] = False
 
 # mysql configuration
 mysql = msl()
@@ -77,33 +82,41 @@ def Badges():
 def Fail_login():
     return tmp("fail_login.html")
 
+@app.route("/fail_login2") # page de redirection login
+def Fail_login2():
+    return tmp("fail_login2.html")
+
 @app.route("/success_login") # page de redirection login
 def Success_login():
     return tmp("success_login.html")
 
 @app.route("/login", methods=['POST', 'GET'])
-def Login():
+def Login(user_name):
     form = LoginForm(rq.form)
-    if rq.method == 'POST': 
+    if rq.method == 'POST':
+        session['user_name'] = user_name
+        name = form.name.data
         user_email = form.email.data
         password = form.password.data
         cur = mysql.connection.cursor()  # connexion à la base de données
-        cur.execute("INSERT INTO login(email_user, password_user) VALUES(%s, %s)", (user_email, password))  # exécution de la requête mysql
+        cur.execute("INSERT INTO login(email_user, password_user, user_name) VALUES(%s, %s, %s)", (user_email, password, name))  # exécution de la requête mysql
         dup1 = cur.execute("select * from inscription where email_user = %s",[user_email])
         dup2 = cur.execute("select * from inscription where password_user = %s",[password])
+        dup3 = cur.execute("select * from inscription where user_name = %s",[name])
 
         # partie vérifification du mdp et de l'utilisateur dans la db
-        if dup1 > 0 and dup2 > 0:
+        if dup1 > 0 and dup2 > 0 and dup3 > 0:
             cur.fetchone()
             mysql.connection.commit()
             cur.close()
             fls("Login successful")
             return rdir(rlf('Success_login')) # redirection avec message si infos valide
+
         else:
             cur.close()
-            fls("Error, email user does not exist, or the password is incorrect") # redirection avec message si infos non existante, ou non valide
+            fls("Error: email user does not exist, the password is incorrect or username is incorrect") # redirection avec message si infos non existante, ou non valide
             return rdir(rlf('Fail_login'))
-    return tmp("login.html", form=form)
+    return tmp(f"Welcome {user_name}", "login.html", form=form)
 
 # partie vérification signup
 class RegistrationForm(fm):
@@ -148,6 +161,15 @@ class RegistrationForm(fm):
 
 # partie vérification login
 class LoginForm(fm):
+    name = stf(
+        "Name : ",
+        [
+            dt(message="Please enter a name"),
+            lg(min=4, message="Name is too short, please try again."),
+            lg(max=40, message="Name is too long, please try again"),
+        ]
+    )
+
     email = emf(
         "Email : ",
         [
